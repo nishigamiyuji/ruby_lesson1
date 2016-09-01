1f23c3e1d0afe8da50ef8935b99d884259def58e
MYCOMPUTER\28410 <2841006@bemax.jp>
2016-09-01 14:14:00 +0900

    復習１

---
+++ lesson1-32.rb
@@ -0,0 +1,24 @@
+# 次の配列を基本選択法で昇順に整列せよ
+
+ary = [34,29,783,73,12,238]
+
+i =0
+while  i<ary.length-1    do
+	min =i
+	j = i+1
+	while  j<ary.length   do
+		if  ary[min]>ary[j]       then
+			min =j
+		end
+	j +=1 
+	end
+	
+	w = ary[min]
+	ary[min] = ary[i]
+	ary[i] = w
+	
+	i +=1 
+end
+
+puts ary
+
